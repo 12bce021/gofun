8d1fd358b8964fde073c20d96e93cb0ad2cbd3ce
12bce021 <12bce021@nirmauni.ac.in>
2018-08-17 22:08:19 +0530

    minor changes

--- route.go
+++ route.go
@@ -1,12 +1,12 @@
-package route
+package main
 import (
 	"fmt"
 	"net/http"
 )
 
-func route() {
+func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website!")
 	})
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8080", nil)
 }
